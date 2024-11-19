6f19ff04def06e39d2ffa1c0b91561858eb9297f
Denis Parfenov <Parfenix@gmail.com>
2024-11-19 20:59:22 +0300

    Initialization of the project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
+# java-project-72
+A site that analyzes specified pages for SEO suitability.
+
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Parfenix/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Parfenix/java-project-72/actions)
+
+[![Actions Status](https://github.com/Parfenix/java-project-72/actions/workflows/main.yml/badge.svg)](https://github.com/Parfenix/java-project-72/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/8b4da7a85ace2af82927/maintainability)](https://codeclimate.com/github/Parfenix/java-project-72/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/8b4da7a85ace2af82927/test_coverage)](https://codeclimate.com/github/Parfenix/java-project-72/test_coverage)
+
+### Requirements:
+* Git
+* Make
+* JDK 21
+* Gradle 8.7
+* 
+### Setup:
+```sh
+$ git clone https://github.com/Parfenix/java-project-72.git && cd java-project-72
+$ make install
+```
+
---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    jacoco
+    checkstyle
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.h2database:h2:2.3.232")
+    implementation("com.zaxxer:HikariCP:6.2.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
+    implementation("org.apache.commons:commons-text:1.12.0")
+    implementation("gg.jte:jte:3.1.15")
+    implementation("org.slf4j:slf4j-simple:2.1.0-alpha1")
+    implementation("io.javalin:javalin:6.3.0")
+    implementation("io.javalin:javalin-bundle:6.3.0")
+    implementation("io.javalin:javalin-rendering:6.3.0")
+
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
