b73f1ea222b428c8f5e66d6b3a9de714062cf021
Denis Parfenov <Parfenix@gmail.com>
2024-11-27 16:34:06 +0300

    JDBC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 
 dependencies {
     implementation("com.h2database:h2:2.3.232")
+    implementation("org.postgresql:postgresql:42.7.4")
     implementation("com.zaxxer:HikariCP:6.2.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
     implementation("org.apache.commons:commons-text:1.12.0")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,18 @@
 package hexlet.code;
 
+import java.io.BufferedReader;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
+@Slf4j
 public class App {
 
     public static void main(String[] args) throws SQLException, IOException {
@@ -14,11 +22,35 @@ public class App {
     }
 
     public static Javalin getApp() throws IOException, SQLException {
+        var hikariConfig = new HikariConfig();
+        var jdbcUrl = System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        hikariConfig.setJdbcUrl(jdbcUrl);
+
+        var dataSource = new HikariDataSource(hikariConfig);
+        var sql = readResourceFile("schema.sql");
+        log.info(sql);
+
+        try (var connection = dataSource.getConnection();
+            var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+
+        BaseRepository.dataSource = dataSource;
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
 
         app.get("/", ctx -> ctx.result("Hello World"));
         return app;
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        var inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+
+        try (var bufferedReader = new BufferedReader(inputStreamReader)) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        }
     }
 }
